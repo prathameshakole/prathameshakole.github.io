ddac27b1396e1342edfa0f181923a0a79e14346b
Prathamesh Akole <akoleprathamesh@gmail.com>
2024-05-05 23:04:30 -0400

    added gh-pages dependency

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,61 +5,56 @@ import Loader from 'react-loaders';
 const Project = () => {
     return (
         <>
-            <div>
-                <div className='heading'>
+            <div className='project-section'>
                     <h1 className='heading'> Projects </h1>
+                    <div className="project-card">
+                        <div className="card-image">
+                            <img src={imageUrl} alt='image' />
+                        </div>
+                        <div className="card-content">
+                            <h3>Image Processing Software</h3>
+                            <p>lorem ipsum asbasoidbasdhfb alsdasdfasdfasdfasdac  afasv asfa vavas fa sdfasdf asdfa sdfdsa gbdfgbsth s sh htafgafgasdfasd ysgdfyas fgasyfgfas faysdgf asdfiu asdifugasdiu fasdhufha sfiuasgfa sjdfba uihrvasjfnva aiusfha dsvnasdjv hasui agusahfusdh ufha sdufh asudfiha fasahf ausdhf au</p>
+                            <a href='#' target="_blank" rel="noopener noreferrer">
+                                Github Link
+                            </a>
+                        </div>
+                    </div>
+                    <div className="project-card">
+                        <div className="card-image">
+                            <img src={imageUrl} alt='image' />
+                        </div>
+                        <div className="card-content">
+                            <h3>Image Processing Software</h3>
+                            <p>lorem ipsum asbasoidbasdhfb alsdasdfasdfasdfasdac  afasv asfa vavas fa sdfasdf asdfa sdfdsa gbdfgbsth s sh htafgafgasdfasd ysgdfyas fgasyfgfas faysdgf asdfiu asdifugasdiu fasdhufha sfiuasgfa sjdfba uihrvasjfnva aiusfha dsvnasdjv hasui agusahfusdh ufha sdufh asudfiha fasahf ausdhf au</p>
+                            <a href='#' target="_blank" rel="noopener noreferrer">
+                                Github Link
+                            </a>
+                        </div>
+                    </div>
+                    <div className="project-card">
+                        <div className="card-image">
+                            <img src={imageUrl} alt='image' />
+                        </div>
+                        <div className="card-content">
+                            <h3>Image Processing Software</h3>
+                            <p>lorem ipsum asbasoidbasdhfb alsdasdfasdfasdfasdac  afasv asfa vavas fa sdfasdf asdfa sdfdsa gbdfgbsth s sh htafgafgasdfasd ysgdfyas fgasyfgfas faysdgf asdfiu asdifugasdiu fasdhufha sfiuasgfa sjdfba uihrvasjfnva aiusfha dsvnasdjv hasui agusahfusdh ufha sdufh asudfiha fasahf ausdhf au</p>
+                            <a href='#' target="_blank" rel="noopener noreferrer">
+                                Github Link
+                            </a>
+                        </div>
+                    </div>
+                    <div className="project-card">
+                        <div className="card-image">
+                            <img src={imageUrl} alt='image' />
+                        </div>
+                        <div className="card-content">
+                            <h3>Image Processing Software</h3>
+                            <p>lorem ipsum asbasoidbasdhfb alsdasdfasdfasdfasdac  afasv asfa vavas fa sdfasdf asdfa sdfdsa gbdfgbsth s sh htafgafgasdfasd ysgdfyas fgasyfgfas faysdgf asdfiu asdifugasdiu fasdhufha sfiuasgfa sjdfba uihrvasjfnva aiusfha dsvnasdjv hasui agusahfusdh ufha sdufh asudfiha fasahf ausdhf au</p>
+                            <a href='#' target="_blank" rel="noopener noreferrer">
+                                Github Link
+                            </a>
+                        </div>
                 </div>
-                <div className='project-card-collection'>
-                    <div className="project-card">
-                        <div className="card-image">
-                            <img src={imageUrl} alt='image' />
-                        </div>
-                        <div className="card-content">
-                            <h3>Image Processing Software</h3>
-                            <p>lorem ipsum asbasoidbasdhfb alsdasdfasdfasdfasdac  afasv asfa vavas fa sdfasdf asdfa sdfdsa gbdfgbsth s sh htafgafgasdfasd ysgdfyas fgasyfgfas faysdgf asdfiu asdifugasdiu fasdhufha sfiuasgfa sjdfba uihrvasjfnva aiusfha dsvnasdjv hasui agusahfusdh ufha sdufh asudfiha fasahf ausdhf au</p>
-                            <a href='#' target="_blank" rel="noopener noreferrer">
-                                Github Link
-                            </a>
-                        </div>
-                    </div>
-                    <div className="project-card">
-                        <div className="card-image">
-                            <img src={imageUrl} alt='image' />
-                        </div>
-                        <div className="card-content">
-                            <h3>Image Processing Software</h3>
-                            <p>lorem ipsum asbasoidbasdhfb alsdasdfasdfasdfasdac  afasv asfa vavas fa sdfasdf asdfa sdfdsa gbdfgbsth s sh htafgafgasdfasd ysgdfyas fgasyfgfas faysdgf asdfiu asdifugasdiu fasdhufha sfiuasgfa sjdfba uihrvasjfnva aiusfha dsvnasdjv hasui agusahfusdh ufha sdufh asudfiha fasahf ausdhf au</p>
-                            <a href='#' target="_blank" rel="noopener noreferrer">
-                                Github Link
-                            </a>
-                        </div>
-                    </div>
-                    <div className="project-card">
-                        <div className="card-image">
-                            <img src={imageUrl} alt='image' />
-                        </div>
-                        <div className="card-content">
-                            <h3>Image Processing Software</h3>
-                            <p>lorem ipsum asbasoidbasdhfb alsdasdfasdfasdfasdac  afasv asfa vavas fa sdfasdf asdfa sdfdsa gbdfgbsth s sh htafgafgasdfasd ysgdfyas fgasyfgfas faysdgf asdfiu asdifugasdiu fasdhufha sfiuasgfa sjdfba uihrvasjfnva aiusfha dsvnasdjv hasui agusahfusdh ufha sdufh asudfiha fasahf ausdhf au</p>
-                            <a href='#' target="_blank" rel="noopener noreferrer">
-                                Github Link
-                            </a>
-                        </div>
-                    </div>
-                    <div className="project-card">
-                        <div className="card-image">
-                            <img src={imageUrl} alt='image' />
-                        </div>
-                        <div className="card-content">
-                            <h3>Image Processing Software</h3>
-                            <p>lorem ipsum asbasoidbasdhfb alsdasdfasdfasdfasdac  afasv asfa vavas fa sdfasdf asdfa sdfdsa gbdfgbsth s sh htafgafgasdfasd ysgdfyas fgasyfgfas faysdgf asdfiu asdifugasdiu fasdhufha sfiuasgfa sjdfba uihrvasjfnva aiusfha dsvnasdjv hasui agusahfusdh ufha sdufh asudfiha fasahf ausdhf au</p>
-                            <a href='#' target="_blank" rel="noopener noreferrer">
-                                Github Link
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
             </div>
             <Loader type="pacman" />
         </>
