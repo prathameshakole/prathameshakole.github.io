0e2d14ca1a30f7a234d16dfd87c8bfc295fb515c
Prathamesh Akole <akoleprathamesh@gmail.com>
2024-06-21 12:56:44 -0400

    changed projects

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,11 +9,11 @@ const Home = () => {
         <>
             <div className='container home-page'>
                 <div className='text-zone'>
-                    <h1>Hi, <br /> I'm<img src={LogoP} alt='developer' />rathamesh,
+                    <h1>Hi, <br /> I'm<img src={LogoP} alt='developer' />rathamesh Akole,
                         <br />
                         Software Engineer.
                     </h1>
-                    <h2>Backend Engineer / Cloud Computing / Machine Learning</h2>
+                    <h2>Full-Stack / Cloud / Data Science / Machine Learning</h2>
                     <Link to='/contact' className='flat-button'>Connect with me!</Link>
                 </div>
                 <Logo />
